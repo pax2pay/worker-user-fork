d3e3c7c264f503e66d46e90208f236318eaeef2f
Simon Mika <simon@mika.se>
2021-08-10 13:06:57 +0200

    Added support for FormData.

--- router.ts
+++ router.ts
@@ -1,3 +1,18 @@
+import { FormData } from "cloud-formdata"
+import * as http from "cloud-http"
 import * as cloudRouter from "cloud-router"
+
+http.Parser.add(
+	async request =>
+		Object.fromEntries(
+			(
+				await FormData.parse(
+					new Uint8Array(await request.arrayBuffer()),
+					request.headers.get("Content-Type") ?? "multipart/form-data"
+				)
+			).entries()
+		),
+	"multipart/form-data"
+)
 
 export const router = new cloudRouter.Router()
